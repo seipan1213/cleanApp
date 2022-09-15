45c72fcdad56af50b4356e03c0738fcbd4b589db
seipan1213 <seiju1213@gmail.com>
2022-09-15 18:57:30 +0900

    remind 実装

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:flutter_native_timezone/flutter_native_timezone.dart';
 import 'package:rakuten_demo/firebase_options.dart';
 import 'package:rakuten_demo/fragments/postPage.dart';
 import 'package:rakuten_demo/pages/loginPage.dart';
@@ -7,12 +9,37 @@ import 'fragments/profile.dart';
 import 'fragments/timeline.dart';
 import 'fragments/cleaningSetting.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:timezone/data/latest.dart' as tz;
+import 'package:timezone/timezone.dart' as tz;
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  tz.initializeTimeZones();
+  final String? timeZoneName = await FlutterNativeTimezone.getLocalTimezone();
+  try {
+    tz.setLocalLocation(tz.getLocation(timeZoneName!));
+  } catch (e) {
+    tz.setLocalLocation(tz.getLocation("Asia/Tokyo"));
+  }
+
+  // flutter_local_notificationsの初期化
+  const AndroidInitializationSettings initializationSettingsAndroid =
+      AndroidInitializationSettings('@mipmap/ic_launcher');
+  const IOSInitializationSettings initializationSettingsIOS =
+      IOSInitializationSettings(
+    requestAlertPermission: true,
+    requestBadgePermission: true,
+    requestSoundPermission: true,
+  );
+  const InitializationSettings initializationSettings = InitializationSettings(
+    android: initializationSettingsAndroid,
+    iOS: initializationSettingsIOS,
+  );
+
+  await FlutterLocalNotificationsPlugin().initialize(initializationSettings);
   runApp(const MyApp());
 }
 
